4968b5b23632f9c5a6f489b4f78bc78f69a47b87
Diana <Khachaturova.Diana@student.greenriver.edu>
2025-04-21 14:02:13 -0700

    Added more comments in RestaurantsContainer.jsx and App.jsx

--- usestate/src/App.jsx
+++ usestate/src/App.jsx
@@ -3,6 +3,7 @@ import { restaurants } from "./data/data.js";
 import { useState } from "react"; 
 import "./App.css";
 
+// App is the root of our application and where we load in our components.
 function App() {
   // creating state using the restaurants array
   const [restaurantState, setRestaurants] = useState([...restaurants]);
--- usestate/src/components/RestaurantsContainer.jsx
+++ usestate/src/components/RestaurantsContainer.jsx
@@ -3,6 +3,7 @@ import Restaurant from "./Restaurant";
 function RestaurantsContainer(props) {
   return (
     <div className="restaurantContainer">
+      {/* rendering a Restaurant component for each item in the restaurants array. */}
       {props.restaurants.map((restaurant) => (
         <Restaurant key={restaurant.id} restaurant={restaurant} />
       ))}
