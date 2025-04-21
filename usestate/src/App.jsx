34fc5167878e7398c65f5bc920d9a409c62f870a
Diana <Khachaturova.Diana@student.greenriver.edu>
2025-04-21 13:38:58 -0700

    Completed steps 1 and 2 of the Learning Practice for state, and added comments in App.jsx

--- usestate/src/App.jsx
+++ usestate/src/App.jsx
@@ -1,13 +1,19 @@
 import RestaurantsContainer from "./components/RestaurantsContainer";
-import { restaurants } from "./data/data.js";
+import { restaurants } from "./data/data.js"; 
+import { useState } from "react"; 
 import "./App.css";
 
 function App() {
+  // creating state using the restaurants array
+  const [restaurantState, setRestaurants] = useState([...restaurants]);
+
   return (
     <div className="App">
-      <RestaurantsContainer />
+      {/* passing down state as props */}
+      <RestaurantsContainer restaurants={restaurantState} />
     </div>
   );
 }
 
 export default App;
+
